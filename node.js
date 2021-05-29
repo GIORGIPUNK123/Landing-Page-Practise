2cc00477411f50b751557d975066e2177592880c
Giorgi Charashvili <giorgirock@hotmail.com>
2021-05-29 21:39:13 +0400

    almost done but needs fixes

---
+++ node.js
@@ -0,0 +1,5 @@
+const slideImage = document.querySelectorAll(".slide-image");
+const slidesContainer = document.querySelector(".slides-container");
+const nextBtn = document.querySelector(".next-btn");
+const prevBtn = document.querySelector(".prev-btn");
+const navigationDots = document.querySelector(".navigation-dots");
